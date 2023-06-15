7d3b22a81ef8ed8a3ade06346046eb311e745ecf
Jakub Wąsowski <jakub@nerdz.pl>
2023-06-15 15:57:34 +0200

    fix: elo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const trains = require("./data/trains.json");
 const app = express()
 
 const PORT = process.env.PORT || 4000
@@ -6,6 +7,208 @@ const PORT = process.env.PORT || 4000
 app.get('/', (req, res) => {
     res.send('I love you Nerdy! Do it, reject my code!')
 })
+
+app.get('/trains', (req, res) => {
+       const trains = require('./data/trains.json')
+           res.json(trains)
+        })
+
+
+
+
+
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+})
+app.get('/trains', (req, res) => {
+    const trains = require('./data/trains.json')
+    res.json(trains)
+}
+
+
+
 
 app.listen(PORT, () => {
     console.log('Server listening on port 3000')
